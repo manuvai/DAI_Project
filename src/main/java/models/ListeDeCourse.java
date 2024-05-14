ab61e56f1bee5303d191bfe0c910d016ac40cd10
Noé <56883291+neo200iq@users.noreply.github.com>
2024-05-14 14:12:21 +0200

    Created Recette, PostIt and ListeDeCourse classes

---
+++ src/main/java/models/ListeDeCourse.java
@@ -0,0 +1,18 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+	@Entity
+	@Table(name = "ListeDeCourse")
+	public class ListeDeCourse {
+		public ListeDeCourse() {}
+		
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Integer id;
+	}
