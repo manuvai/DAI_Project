36024f336530c20fe31abbd4e779a46e7f4dc61a
Manuvai <44281637+manuvai@users.noreply.github.com>
2024-05-21 15:35:35 +0200

    feat: Inversion de l'ordre des produits

--- src/main/java/servlets/ManagementServlet.java
+++ src/main/java/servlets/ManagementServlet.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -58,6 +59,7 @@ public class ManagementServlet extends AbstractServlet {
 			throws ServletException, IOException {
 
 		final List<Article> articles = articleRepository.findAll();
+		Collections.reverse(articles);
 		request.setAttribute("articles", articles);
 		view("management/index", request, response);
 
