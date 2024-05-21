0505af142537d795c59ba0c8ae381d33710fa6ef
Elisa Cueille <elisacueille1109@gmail.com>
2024-05-21 21:25:23 +0200

    suppresion panier et mise en forme de fichier

--- src/main/java/servlets/ValiderPanierServlet.java
+++ src/main/java/servlets/ValiderPanierServlet.java
@@ -23,49 +23,54 @@ import repositories.MagasinRepository;
 @WebServlet("/ValiderPanierServlet")
 public class ValiderPanierServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    ArticleRepository articleRepository = new ArticleRepository();
-    MagasinRepository magasinRepository = new MagasinRepository();
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ValiderPanierServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	ArticleRepository articleRepository = new ArticleRepository();
+	MagasinRepository magasinRepository = new MagasinRepository();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ValiderPanierServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		ArrayList<String> numeros = (ArrayList<String>) session.getAttribute("numeros");
 		ArrayList<Magasin> magasins = (ArrayList<Magasin>) magasinRepository.findAll();
 		session.setAttribute("tousMagasins", magasins);
-		
+
 		Utilisateur user = (Utilisateur) session.getAttribute("user");
-		 if(user != null) {
-			 double total = 0;
-				
-				for(String num: numeros) {
-					Article a = (Article) articleRepository.findById(Integer.parseInt(num));
-					int qte = (int) session.getAttribute(num);
-					total = total + a.getPrixUnitaire()*qte*(1-a.getPromotion()/100);
-				}
-				session.setAttribute("totalPanierValidation", total);
-				
-				RequestDispatcher rd = request.getRequestDispatcher("validerPanier");
-				rd.forward(request, response);
-		 }else {
-			 RequestDispatcher rd = request.getRequestDispatcher("connexion");
-				rd.forward(request, response);
-		 }
-		
+		if (user != null) {
+			double total = 0;
+
+			for (String num : numeros) {
+				Article a = (Article) articleRepository.findById(Integer.parseInt(num));
+				int qte = (int) session.getAttribute(num);
+				total = total + a.getPrixUnitaire() * qte * (1 - a.getPromotion() / 100);
+			}
+			session.setAttribute("totalPanierValidation", total);
+
+			RequestDispatcher rd = request.getRequestDispatcher("validerPanier");
+			rd.forward(request, response);
+		} else {
+			RequestDispatcher rd = request.getRequestDispatcher("connexion");
+			rd.forward(request, response);
+		}
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
--- src/main/webapp/js/promotion.js
+++ src/main/webapp/js/promotion.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	const checkbox = document.getElementById("utiliserPoints");
     checkbox.addEventListener("change", () => {
 		
+		//recuperation des valeurs
 
     	const totalPP = document.getElementById("totalPP");
     	const ptFidel = parseInt(document.getElementById("ptFidel").textContent.trim(), 10);
@@ -12,13 +13,16 @@ document.addEventListener("DOMContentLoaded", () => {
         let reduction = Math.floor(ptFidel / 10);
     
 		
+		//observe si la checkbox est cochee
         if (checkbox.checked) {
+			//applique une reduction si c'est la cas
 			if (reduction > total){
 				reduction = Math.floor(total);
 			}
             const nouveauTotal = total - reduction;
             totalPP.textContent = nouveauTotal.toFixed(2);
             ptUsedElement.textContent = reduction*10;
+            //ou remet le prix ultérieur
         } else {   			    
             const ancienTotal = total + ptUsed/10;
             totalPP.textContent = ancienTotal.toFixed(2);
