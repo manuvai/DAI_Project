ef2a1619c74ca101af8e8b57fac707525769ed25
Elisa Cueille <elisacueille1109@gmail.com>
2024-05-23 11:50:53 +0200

    feature(changer etet cmd et alimenter le stock)

--- src/main/java/servlets/ValiderRecuCommande.java
+++ src/main/java/servlets/ValiderRecuCommande.java
@@ -10,10 +10,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import models.Approvisionner;
 import models.Article;
 import models.Commande;
 import models.Commande.Etat;
+import models.keys.StockerKey;
 import models.Magasin;
 import models.Stocker;
 import repositories.CommandeRepository;
@@ -41,20 +45,48 @@ public class ValiderRecuCommande extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String idStr = request.getParameter("id");
         int id = Integer.parseInt(idStr);
-        Commande commande = cr.findById(id);
         
+        //recuperation commande
+        Session session = cr.getSession();
+        Transaction t =  session.beginTransaction();
+        Commande commande = cr.findById(id, session);
+       System.out.println("ccmdmdm");
+       System.out.println(commande.getId());
+        
+        //articles de la commande
         Map<Article, Approvisionner> articlesAdd = commande.getArticleApprovisionner();
         
+        //magasin
         Magasin magasin = commande.getMagasin();
+        
+        //stock actuel
         Map<Article, Stocker> stockers = magasin.getStockers();
-   
+        
+        for (Map.Entry<Article, Approvisionner> entry : articlesAdd.entrySet()) {
+            Article article = entry.getKey();
+            int quantityToAdd = entry.getValue().getQte();
 
+            Stocker stocker = stockers.get(article);
+            if (stocker != null) {
+                // If the article is already in stock, increase the quantity
+                stocker.setQuantite(stocker.getQuantite() + quantityToAdd);
+            } else {
+                // If the article is not in stock, create a new stocker entry
+                stocker = new Stocker();
+                stocker.setQuantite(quantityToAdd);
+                StockerKey stockerKey = new StockerKey(magasin.getCodeMagasin(), article.getId());
+                stocker.setKey(stockerKey);
+                stockers.put(article, stocker);
+                
+            }
+        }
         // Update the commande status to 'LIVREE'
         commande.setEtat(Etat.LIVREE);
 
         // Save changes to the database
-        cr.update(commande);
-        RequestDispatcher rd = request.getRequestDispatcher("detailsCommandeFournisseur");
+        cr.update(commande, session);
+        t.commit();
+        RequestDispatcher rd = request.getRequestDispatcher("voirCommande");
 		rd.forward(request, response);
 	}
 
